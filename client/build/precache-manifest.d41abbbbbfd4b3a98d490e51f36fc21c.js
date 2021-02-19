af0e003719b431b35b24b32e25c74db8d218dfcd
Nadia Mazzoni <Nadia@Nadias-MBP.lan>
2021-02-19 18:29:17 +0100

    build folder added

---
+++ client/build/precache-manifest.d41abbbbbfd4b3a98d490e51f36fc21c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e62ee276496d8be1e087276f955b9857",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e01364ab125ec19ea7f8",
+    "url": "/static/css/main.cb3c7cbe.chunk.css"
+  },
+  {
+    "revision": "31c7b81a30a3c67657e7",
+    "url": "/static/js/2.30d84bee.chunk.js"
+  },
+  {
+    "revision": "1a1a42c150bd05496b97b265c552c789",
+    "url": "/static/js/2.30d84bee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e01364ab125ec19ea7f8",
+    "url": "/static/js/main.ba1607db.chunk.js"
+  },
+  {
+    "revision": "2c7397a514ae13184ca1",
+    "url": "/static/js/runtime-main.ca3c6dad.js"
+  },
+  {
+    "revision": "e433a018a243bd7066b0e2a89213c6f8",
+    "url": "/static/media/illustration.e433a018.svg"
+  }
+]);
